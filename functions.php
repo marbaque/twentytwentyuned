a8b3bc3bed2af80f5314dd1c73af0df107e13940
mbadilla <mario.badilla@gmail.com>
2022-01-31 16:08:19 -0600

    first commit

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'twenty_child_enqueue_styles' );
+function twenty_child_enqueue_styles() {
+    $parenthandle = 'twentytwenty'; // This is 'twentytwenty' for the Twenty Twenty theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(),  // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('1.0.0') // this only works if you have Version in the style header
+    );
+}
+
+require 'assets/plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/marbaque/twentytwentyuned',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'twentytwentyuned'
+);
+$myUpdateChecker->setBranch('main');
